08be67b27b87f186d17d7ecfb3a91ae4cdf2ca22
Staff Author <waltritscher@outlook.com>
2019-09-19 16:18:29 -0700

    Ready to start

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -25,38 +25,50 @@ namespace ExampleApp
 			InitializeComponent();
 		}
 
-	
-		
+
+		private void ButtonA_Click(object sender, RoutedEventArgs e)
+		{
+			Char c1 = '.';
+			Char c2 = '3';
+			// How would you test for punctuation?
+			if (c1 == '.' || c1 == '!' || c1 == ',' || c1 == ';' || c1 == ':' || c1 == '?')
+			{
+				// do something
+			}
+
+			// How do you test for numeric?
+			if (c2 == '0' || c2 == '1' || c2 == '2' || c2 == '3' || c2 == '4' || c2 == '5')
+			{
+				// do something
+			}
+		}
 
 
 		//	https://docs.microsoft.com/en-us/dotnet/api/system.char.ispunctuation?view=netframework-4.8
-	
-	private void ButtonA_Click(object sender, RoutedEventArgs e) {
-			Char c1 = '.';
-		// use the Char methods to test for Unicode categories.
-		
-		if (Char.IsPunctuation(c1))
+		private void ButtonB_Click(object sender, RoutedEventArgs e)
 		{
-			// determine if this char is considered a punctuation
-			// by the Unicode Standards.
+			Char c1 = '.';
+			// use the Char methods to test for Unicode categories.
+
+			if (Char.IsPunctuation(c1))
+			{
+				// determine if this char is considered a punctuation
+				// by the Unicode Standards.
+			}
 		}
-		
-		}
 
-
-		private void ClearButton_Click(object sender, RoutedEventArgs e) {
-			OutputTextBox.Text = string.Empty;
-		}
-
-	
-
-		private void InputTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) {
+		private void InputTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
 			Char currentChar = e.Text[0];
 
 			if (Char.IsDigit(currentChar))
 			{
 				e.Handled = true;
 			}
+		}
+		private void ClearButton_Click(object sender, RoutedEventArgs e)
+		{
+			OutputTextBox.Text = string.Empty;
 		}
 	}
 }
