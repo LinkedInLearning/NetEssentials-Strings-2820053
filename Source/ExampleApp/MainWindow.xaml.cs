a4fafeb40299f9fff564cd6243c391d60436b576
Walt Ritscher <waltritscher@outlook.com>
2019-09-18 18:50:11 -0700

    Add constructor code

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -13,16 +13,49 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ExampleApp
-{
+namespace ExampleApp {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
+	public partial class MainWindow : Window {
+		public MainWindow() {
 			InitializeComponent();
 		}
+
+		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+
+			// use string constructors
+
+
+			var s1 = new String('=', 400);
+			OutputTextBox.Text = s1;
+		}
+
+		private void ButtonB_Click(object sender, RoutedEventArgs e) {
+			var db = new ProduceDB();
+			var s1 = new String(db.GetFruits());
+			OutputTextBox.Text = s1;
+		}
+
+		private void ClearButton_Click(object sender, RoutedEventArgs e) {
+			OutputTextBox.Text = string.Empty;
+		}
+
+		private void ButtonC_Click(object sender, RoutedEventArgs e) {
+			var db = new ProduceDB();
+			var s1 = new String(db.GetFruits(),7,3);
+			OutputTextBox.Text = s1;  
+			s1 = new string ()
+		}
+	}
+	public class ProduceDB {
+		public char[] GetFruits() {
+			// for example, a legacy service in your enterprise
+			// that returns char arrays
+			char[] fruits = { 'b', 'a', 'n', 'a', 'n', 'a', ',', 'f', 'i', 'g' };
+
+			return fruits;
+		}
+
 	}
 }
