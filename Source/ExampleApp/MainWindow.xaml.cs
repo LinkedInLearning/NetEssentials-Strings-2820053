6d1c6284e1316ad6848002b45895a813a7cfd6f8
Walt Ritscher <waltritscher@outlook.com>
2019-09-19 13:24:40 -0700

    Add verbatim code examples

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -25,8 +25,37 @@ namespace ExampleApp
 			InitializeComponent();
 		}
 
+
+		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+			// There is no runtime difference between a string and verbatim string. 
+			// The C# compiler handles verbatim strings, performing extra operations 
+
+			string s1;
+
+			s1 = "A file path\vC:\\Windows\\Users";
+			OutputTextBox.Text = s1;
+		}
+
+
 		private void ClearButton_Click(object sender, RoutedEventArgs e) {
 			OutputTextBox.Text = string.Empty;
+		}
+
+		private void ButtonB_Click(object sender, RoutedEventArgs e) {
+			string s2;
+
+			s2 = @"A file path\vC:\\Windows\\Users";
+			OutputTextBox.Text = s2;
+		}
+
+		private void ButtonC_Click(object sender, RoutedEventArgs e) {
+			string s3;
+			// the C# compiler adds \r\n for each newline in the verbatim string
+			s3 = @"Select ProductName Price
+             From Products
+             Where price >23
+             Order By Price";
+			OutputTextBox.Text = s3;
 		}
 	}
 }
