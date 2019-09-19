d417d608220e443f7766809e52d3408de42985e7
Staff Author <waltritscher@outlook.com>
2019-09-19 14:20:58 -0700

    Ready to start

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -13,43 +13,53 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ExampleApp {
+namespace ExampleApp
+{
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow() {
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
 			InitializeComponent();
 		}
 
-		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+		private void ButtonA_Click(object sender, RoutedEventArgs e)
+		{
 
 			// use string constructors
+			string s1 = String.Empty;
 
-
-			var s1 = new String('=', 400);
 			OutputTextBox.Text = s1;
 		}
 
-		private void ButtonB_Click(object sender, RoutedEventArgs e) {
-			var db = new ProduceDB();
-			var s1 = new String(db.GetFruits());
-			OutputTextBox.Text = s1;
+		private void ButtonB_Click(object sender, RoutedEventArgs e)
+		{
+			var db = new GroceryDb();
+			string s2 = string.Empty;
+
+
+			OutputTextBox.Text = s2;
 		}
 
-		private void ClearButton_Click(object sender, RoutedEventArgs e) {
+		private void ButtonC_Click(object sender, RoutedEventArgs e)
+		{
+			var db = new GroceryDb();
+			string s3 = String.Empty;
+
+			OutputTextBox.Text = s3;
+
+		}
+		private void ClearButton_Click(object sender, RoutedEventArgs e)
+		{
 			OutputTextBox.Text = string.Empty;
 		}
-
-		private void ButtonC_Click(object sender, RoutedEventArgs e) {
-			var db = new ProduceDB();
-			var s1 = new String(db.GetFruits(),7,3);
-			OutputTextBox.Text = s1;  
-			s1 = new string ()
-		}
 	}
-	public class ProduceDB {
-		public char[] GetFruits() {
+	public class GroceryDb
+	{
+		public char[] GetFruits()
+		{
 			// for example, a legacy service in your enterprise
 			// that returns char arrays
 			char[] fruits = { 'b', 'a', 'n', 'a', 'n', 'a', ',', 'f', 'i', 'g' };
