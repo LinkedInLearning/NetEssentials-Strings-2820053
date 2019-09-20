ce89da37c7973d2e5267423e9a3bb54e715ea281
Walt Ritscher <waltritscher@outlook.com>
2019-09-20 15:46:59 -0700

    Add second example

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -47,7 +47,15 @@ namespace ExampleApp
 		}
 
 		private void ButtonB_Click(object sender, RoutedEventArgs e) {
+			string first = "1. The Banana is yellow.";
+			string second = "2. The pear is ripe.";
 			
+			string florette = " ✿ ";
+
+			string combo = string.Empty;
+
+			combo = $"{first} -- {florette} -- {second}";
+			OutputTextBox.Text = combo;
 		}
 
 		private void ClearButton_Click(object sender, RoutedEventArgs e)
