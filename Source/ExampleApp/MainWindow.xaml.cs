ee6a91be19e1a455f5bbf56dc492433506d2f428
Walt Ritscher <waltritscher@outlook.com>
2019-09-21 10:10:55 -0700

    Update UI

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -29,6 +29,46 @@ namespace ExampleApp
 			OutputTextBox.Text = string.Empty;
 		}
 
+		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+			string foodsA = "apple banana cherry durian eggplant fig grape honey ice";
 
+			string foodsB = "kiwi, lemon, mushroom, onion, potato, radish, spinach, tomato";
+
+			string foodsC = "kiwi--banana--mushroom # eggplant # potato # grape.spinach.honey";
+
+			//  split into a string array
+
+			var foodArrayA = foodsA.Split();
+
+			string[] comma = { ", " };
+			var foodArrayB = foodsB.Split(comma, StringSplitOptions.None);
+
+			string[] splitters = { ", ", "--", ".", " # " };
+
+			var foodArrayC = foodsC.Split(splitters, StringSplitOptions.None);
+		}
+
+		private void ButtonB_Click(object sender, RoutedEventArgs e) {
+			string foodsB = "kiwi, , mushroom, onion, potato, , spinach, tomato";
+
+			string[] comma = { ", " };
+			var foodArrayB = foodsB.Split(comma, StringSplitOptions.RemoveEmptyEntries);
+	
+		}
+
+		private void ButtonC_Click(object sender, RoutedEventArgs e) {
+			string foodsB = "kiwi, lemon, kiwi, onion, potato, lemon, spinach, tomato, lemon";
+			string[] comma = { ", " };
+
+			
+			var foodArrayB = foodsB.Split(comma, StringSplitOptions.RemoveEmptyEntries);
+			var q = foodArrayB.GroupBy(x => x).Select(x=> new {FoodName=x.Key,WordCount = x.Count() });
+
+			foreach (var item in q)
+			{
+				OutputTextBox.Text += $"{item.FoodName}: {item.WordCount}\r\n";
+			}
+			
+		}
 	}
 }
