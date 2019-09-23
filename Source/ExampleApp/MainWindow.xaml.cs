87056d3b0400e7364ec31165e4b6c92347c05209
Walt Ritscher <waltritscher@outlook.com>
2019-09-22 22:27:54 -0700

    Sample code

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -13,15 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ExampleApp
-{
+namespace ExampleApp {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
+	public partial class MainWindow : Window {
+		public MainWindow() {
 			InitializeComponent();
 		}
 
@@ -29,6 +26,69 @@ namespace ExampleApp
 			OutputTextBox.Text = string.Empty;
 		}
 
+		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+			string sample = "At Roux Academy, our mission is to teach and inspire the next generation’s artists...";
+			string startWord = "our";
+			string endWord = "the ";
 
+			string result = string.Empty;
+			result = GetSubStringBetweenWords(sample, startWord, endWord);
+
+			result = ReplaceBetweenWords(sample, startWord, endWord, " quest is to inspire and mentor ");
+
+			OutputTextBox.Text = result;
+		}
+
+		public string GetSubStringBetweenWords(string candidate, string firstWord, string lastWord) {
+			string result = string.Empty;
+			int firstSnipPointIndex, lastSnipPointIndex;
+
+			if (DoesFirstAndLastWordExist(candidate, firstWord, lastWord))
+			{
+				firstSnipPointIndex = candidate.IndexOf(value: firstWord, 
+																				startIndex: 0) + firstWord.Length;
+				lastSnipPointIndex = candidate.IndexOf(value: lastWord, 
+																			startIndex: firstSnipPointIndex);
+				return candidate.Substring(startIndex: firstSnipPointIndex, 
+																		length: lastSnipPointIndex - firstSnipPointIndex);
+			}
+			else
+			{
+				return string.Empty;
+			}
+
+		}
+		private bool DoesFirstAndLastWordExist(string candidate, string firstWord, string lastWord) {
+			return (candidate.Contains(firstWord) && candidate.Contains(lastWord));
+
+		}
+		public string ReplaceBetweenWords(string candidate, string firstWord, string lastWord, string replacementText) {
+			int firstSnipPointIndex, lastSnipPointIndex;
+
+			// "this is " "the new value " "in the string"
+
+			if (DoesFirstAndLastWordExist(candidate, firstWord, lastWord))
+			{
+				firstSnipPointIndex = candidate.IndexOf(value: firstWord, 
+																						startIndex: 0) + firstWord.Length;
+				string firstSubstring = candidate.Substring(startIndex: 0, 
+																								length: firstSnipPointIndex);
+
+				lastSnipPointIndex = candidate.IndexOf(value: lastWord, 
+																					startIndex: firstSnipPointIndex);
+				
+				// use remove or substring to get the end part of the string!
+				string lastSubString = candidate.Remove(startIndex: 0, 
+																								count: lastSnipPointIndex);
+				lastSubString = candidate.Substring(startIndex: lastSnipPointIndex, 
+																						length: candidate.Length - lastSnipPointIndex);
+
+				return $"{firstSubstring}{replacementText}{lastSubString}"; ;
+			}
+			else
+			{
+				return string.Empty;
+			}
+		}
 	}
 }
