968223ab4ed7693b1b418d04d33fbf71c32d94ab
Staff Author <waltritscher@outlook.com>
2019-09-24 01:05:17 -0700

    Recorded successfully

--- Source/ExampleApp/RayPoint.cs
+++ Source/ExampleApp/RayPoint.cs
@@ -18,16 +18,20 @@ namespace ExampleApp {
 			
 		}
 
+
+
 		public string ToString(string format, IFormatProvider formatProvider)
 		{
-			// use this implementation to 
-			// process other cultures (passed via the IFormatProvider)
-			// move the code here from other ToString("H")
+
+			var cultureInfo = formatProvider as System.Globalization.CultureInfo;
+		
+			var separator = cultureInfo.TextInfo.ListSeparator;
+			var numSeparator = cultureInfo.NumberFormat.NumberDecimalSeparator;
 			string formattedString;
 			switch (format)
 			{
 				case "C":
-					formattedString = $"X: {X}, Y:{Y}";
+					formattedString = $"X: {X}{separator} Y:{Y}";
 					break;
 				case "H":
 					formattedString = $"X: {X} - Y:{Y}";
@@ -36,35 +40,11 @@ namespace ExampleApp {
 					formattedString = $"X: {X} --++-- Y:{Y}";
 					break;
 				default:
-					formattedString = $"X: {X}, Y:{Y}";
+					formattedString = $"X: {X}{separator} Y:{Y}";
 					break;
 			}
 			return formattedString;
 		}
-
-		//public string ToString(string format, IFormatProvider formatProvider) {
-
-		//	var cultureInfo = formatProvider as System.Globalization.CultureInfo;
-		//	var separator = cultureInfo.TextInfo.ListSeparator;
-		//	var numSeparator = cultureInfo.NumberFormat.NumberDecimalSeparator;
-		//	string formattedString;
-		//	switch (format)
-		//	{
-		//		case "C":
-		//			formattedString = $"X: {X}{separator} Y:{Y}";
-		//			break;
-		//		case "H":
-		//			formattedString = $"X: {X} - Y:{Y}";
-		//			break;
-		//		case "F":
-		//			formattedString = $"X: {X} --++-- Y:{Y}";
-		//			break;
-		//		default:
-		//			formattedString = $"X: {X}{separator} Y:{Y}";
-		//			break;
-		//	}
-		//	return formattedString;
-		//}
 
 		#region Constructors
 		public RayPoint() {
