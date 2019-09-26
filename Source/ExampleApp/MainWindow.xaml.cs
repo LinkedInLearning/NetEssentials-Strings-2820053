f3fc577698a4bb64fae499a70959f4db732bca05
Walt Ritscher <waltritscher@outlook.com>
2019-09-25 19:41:10 -0700

    Add date formatting

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -32,19 +32,39 @@ namespace ExampleApp
 
 		private void ButtonA_Click(object sender, RoutedEventArgs e)
 		{
-			// "0"		Zero placeholder
-			// "#"		Digit placeholder
-			//  "."		Decimal point
+			// https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+			// "d"	Short date pattern.
+			// "D"	Long date pattern.
 
-			double currentValue = 3355.752;
-			string formattedString = String.Empty;
-			// predefined
-			formattedString = currentValue.ToString("E");
-			OutputToScreen(formattedString);
-			// with custom placeholders
-			formattedString = currentValue.ToString("0.###000E+000"); 
 
-			OutputToScreen(formattedString);
+			DateTime date1 = new DateTime(year: 2002, month: 7, day: 29, hour: 9, minute: 19, second: 29);
+
+			OutputToScreen(date1.ToString());
+		
+			try
+			{
+			
+				OutputToScreen($"Short date:\t(D)\t{date1:D}");
+				OutputToScreen($"Long date\t(d)\t{date1:d}");
+				OutputToScreen($"Long time\t(T)\t{date1:T}");
+				OutputToScreen($"Short time\t(t)\t{date1:t}");
+
+				OutputToScreen($"General\t(G)\t{date1:G}");
+				OutputToScreen($"Month Day\t(M)\t{date1:M}");
+				OutputToScreen($"Year\t(Y)\t{date1:Y}");
+				OutputToScreen($"Sortable\t(s)\t{date1:s}");
+				OutputToScreen($"Sortable\t(s)\t{date1.AddDays(2):s}");
+
+
+
+			} catch (Exception)
+			{
+
+				OutputToScreen("Cannot use that formatter.");
+			}
+		
+
+			
 
 		}
 		private void OutputToScreen(string message)
@@ -52,27 +72,24 @@ namespace ExampleApp
 			OutputTextBox.Text += message + "\r\n";
 		}
 
-		private void Buttonb_Click(object sender, RoutedEventArgs e)
-		{
-			
-			// "D" or "d"	Decimal
-			// "E" or "e"	Exponential
-			// "F" or "f"	Fixed-point
-			// "G" or "g"	General
-			// "N" or "n"	Number
-			// "P" or "p"	Percent
-			// "X" or "x"	Hexadecimal
+		private void ButtonB_Click(object sender, RoutedEventArgs e) {
+			// https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
 
-			double currentValue = Double.Parse(InputTextBox.Text);
-			string formatter = FormatterTextBox.Text;
+			DateTime date1 = new DateTime(year: 2002, month: 7, day: 29, hour: 9, minute: 19, second: 29);
+			OutputToScreen(date1.ToString());
 			try
 			{
-				string formattedString = currentValue.ToString($"{ formatter}");
-				OutputToScreen(formattedString);
-			} catch (Exception ex)
-			{
-				OutputToScreen($"Cannot format {ex.Message}");
 
+				OutputToScreen($"dd-MM-yy:\t\t{date1:dd-MM-yy}");
+				OutputToScreen($"mm^HH^ss:\t\t{date1:mm^HH^ss}");
+				OutputToScreen($"Year:(yyyy) \"Month\"(MM):\t{date1:Year:(yyyy) \"Month\"(MM)}");
+				
+
+
+			} catch (Exception)
+			{
+
+				OutputToScreen("Cannot use that formatter.");
 			}
 		}
 	}
