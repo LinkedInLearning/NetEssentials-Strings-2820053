d2a5ae96d17d93830bf9522dba7b3b6a8e417813
Walt Ritscher <waltritscher@outlook.com>
2019-09-25 20:37:43 -0700

    Add challenge code example

---
+++ Source/ExampleApp/FormatExtension.cs
@@ -0,0 +1,30 @@
+﻿namespace ExampleApp {
+
+	public static class FormatExtension {
+
+		public static string FormatAsOrdinal(this int number) {
+			// 1st, 2nd, 3rd, 4th, 5th, 6th, 7th, 9th, 10th
+			// 21st, 22nd, 23rd, 24th, etc
+			// 31st, 32nd, 33rd, 34th, etc
+			// 11th, 12th, 13th are special cases
+
+			// Walt's thought for the day!
+			// why don't we say tenny-one, tenny-two, tenny-three, tenny-four etc.
+
+			string numString = number.ToString();
+			string ordinalString = string.Empty;
+
+			// process the special cases first
+			if (numString.EndsWith("11")) return $"{numString}th";
+			if (numString.EndsWith("12")) return $"{numString}th";
+			if (numString.EndsWith("13")) return $"{numString}th";
+			// process the rest
+			if (numString.EndsWith("1")) return $"{numString}st";
+			if (numString.EndsWith("2")) return $"{numString}nd";
+			if (numString.EndsWith("3")) return $"{numString}rd";
+
+			// all other numbers
+			return $"{numString}th";
+		}
+	}
+}
--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ExampleApp
-{
+namespace ExampleApp {
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
+	public partial class MainWindow : Window {
+
+		public MainWindow() {
 			InitializeComponent();
 		}
 
@@ -29,6 +16,11 @@ namespace ExampleApp
 			OutputTextBox.Text = string.Empty;
 		}
 
-
+		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+			foreach (var num in Enumerable.Range(1, 30))
+			{
+				OutputTextBox.Text += $"{num.FormatAsOrdinal()}\r\n";
+			}
+		}
 	}
 }
