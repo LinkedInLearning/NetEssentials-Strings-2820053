45574b8eccbfe4720cb11257e4a92a7849ab9a55
Walt Ritscher <waltritscher@outlook.com>
2019-09-25 17:25:11 -0700

    Add conditional formatter examples

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -13,15 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ExampleApp
-{
+namespace ExampleApp {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
+	public partial class MainWindow : Window {
+		public MainWindow() {
 			InitializeComponent();
 		}
 
@@ -29,6 +26,43 @@ namespace ExampleApp
 			OutputTextBox.Text = string.Empty;
 		}
 
+		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+			// The semicolon(;) is a conditional format specifier
 
+			// No semicolon: the format applies all values
+			// 1 semicolon:  Positive and Zero/Negative
+			// 2 semicolon:  Positive/Negative/Zero
+
+			double currentValue = Double.Parse(InputTextBox.Text);
+			string formatter1 = "#,###;(#,###)";
+			string formatter2 = "#,###;(#,###);**Zero**";
+
+			try
+			{
+
+			string	formattedString = currentValue.ToString($"{ formatter2}");
+				OutputToScreen(formattedString);
+			} catch (Exception ex)
+			{
+				OutputToScreen($"Cannot format {ex.Message}");
+
+			}
+		}
+		private void OutputToScreen(string message) {
+			OutputTextBox.Text += message + "\r\n";
+		}
+
+		private void ButtonB_Click(object sender, RoutedEventArgs e) {
+			List<int> numbers = new List<int> {2,4,0,-6,-8};
+			OutputToScreen($"Always show number signs.");
+			
+
+			foreach (var number in numbers)
+			{
+		
+				OutputToScreen($"{number:+#;-#;0} ");
+			}
+		
+		}
 	}
 }
