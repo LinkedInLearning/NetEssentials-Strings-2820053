4fb4d178adeae2e53a9e8ead8aad7e06d1a28e17
Walt Ritscher <waltritscher@outlook.com>
2019-09-27 16:02:39 -0700

    add toString format example code

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -15,73 +15,45 @@ namespace ExampleApp {
 
 		private CultureInfo _cultureUS = new CultureInfo("en-US");
 		private CultureInfo _cultureDE = new CultureInfo("de-DE");
-		private CultureInfo _cultureInvariant = new CultureInfo("");
+		private CultureInfo _cultureBN = new CultureInfo("bn-IN");
+		private CultureInfo _cultureSG = new CultureInfo("zh-CN");
 		private Decimal _saleAmountUS, _saleAmountDE;
 		private string _amountAsUsCurrency1, _amountAsDeCurrency1, _amountAsInvariantCurrency1;
 		private string _amountAsUsCurrency2, _amountAsDeCurrency2, _amountAsInvariantCurrency2;
 
 		private void ButtonA_Click(object sender, RoutedEventArgs e) {
-			try
-			{
-				ParseDataUS();
-				OutputTextBox1.Text += $"US:\t\t{_amountAsUsCurrency1}\r\n";
-				OutputTextBox1.Text += $"DE:\t\t{_amountAsDeCurrency1}\r\n";
-				OutputTextBox1.Text += $"Invariant:\t{_amountAsInvariantCurrency1}\r\n";
-			} catch (Exception)
-			{
-				throw;
-			}
+			decimal saleAmount = 36450.89M;
+			OutputTextBox1.Text += $"{_cultureUS.EnglishName}:\t{saleAmount.ToString("C", _cultureUS)}\r\n";
+			OutputTextBox1.Text += $"{_cultureDE.EnglishName}:\t{saleAmount.ToString("C", _cultureDE)}\r\n";
+			OutputTextBox1.Text += $"{_cultureBN.EnglishName}:\t\t{saleAmount.ToString("C", _cultureBN)}\r\n";
+			OutputTextBox1.Text += $"{_cultureSG.EnglishName}:\t{saleAmount.ToString("C", _cultureSG)}\r\n";
 		}
-
-	
 
 		private void ButtonB_Click(object sender, RoutedEventArgs e) {
-			try
-			{
-				ParseDataDE();
+			DateTime saleDate = DateTime.Parse("7/21/2002 13:15:58", _cultureUS);
 
-				OutputTextBox2.Text += $"US:\t\t{_amountAsUsCurrency2}\r\n";
-				OutputTextBox2.Text += $"DE:\t\t{_amountAsDeCurrency2}\r\n";
-				OutputTextBox2.Text += $"Invariant:\t{_amountAsInvariantCurrency2}\r\n";
-			} catch (Exception)
-			{
-				throw;
-			}
+			OutputTextBox2.Text += $"{_cultureUS.EnglishName}:\t{saleDate.ToString("D", _cultureUS)}\r\n";
+			OutputTextBox2.Text += $"{_cultureDE.EnglishName}:\t{saleDate.ToString("D", _cultureDE)}\r\n";
+			OutputTextBox2.Text += $"{_cultureBN.EnglishName}:\t\t{saleDate.ToString("D", _cultureBN)}\r\n";
+			OutputTextBox2.Text += $"{_cultureSG.EnglishName}:\t{saleDate.ToString("D", _cultureSG)}\r\n";
+
+			OutputTextBox2.Text += $"--------------\r\n";
+
+			OutputTextBox2.Text += $"{_cultureUS.EnglishName}:\t{saleDate.ToString("d", _cultureUS)}\r\n";
+			OutputTextBox2.Text += $"{_cultureDE.EnglishName}:\t{saleDate.ToString("d", _cultureDE)}\r\n";
+			OutputTextBox2.Text += $"{_cultureBN.EnglishName}:\t\t{saleDate.ToString("d", _cultureBN)}\r\n";
+			OutputTextBox2.Text += $"{_cultureSG.EnglishName}:\t{saleDate.ToString("d", _cultureSG)}\r\n";
+
+			OutputTextBox2.Text += $"--------------\r\n";
+			OutputTextBox2.Text += $"{_cultureUS.EnglishName}:\t{saleDate.ToString("T", _cultureUS)}\r\n";
+			OutputTextBox2.Text += $"{_cultureDE.EnglishName}:\t{saleDate.ToString("T", _cultureDE)}\r\n";
+			OutputTextBox2.Text += $"{_cultureBN.EnglishName}:\t\t{saleDate.ToString("T", _cultureBN)}\r\n";
+			OutputTextBox2.Text += $"{_cultureSG.EnglishName}:\t{saleDate.ToString("T", _cultureSG)}\r\n";
 		}
 
-		private void ParseDataUS() {
-			_saleAmountUS = Decimal.Parse(InputTextBox.Text, NumberStyles.Any, _cultureUS);
-			_amountAsUsCurrency1 = _saleAmountUS.ToString("C", _cultureUS);
-			_amountAsDeCurrency1 = _saleAmountUS.ToString("C", _cultureDE);
-			_amountAsInvariantCurrency1 = _saleAmountUS.ToString("C", _cultureInvariant); ;
+		nputTextBoxDE.Text = "4.615,44";
 		}
 
-		private void ParseDataDE() {
-			_saleAmountDE = Decimal.Parse(InputTextBoxDE.Text, NumberStyles.Any, _cultureDE);
-			_amountAsUsCurrency2 = _saleAmountDE.ToString("C", _cultureUS);
-			_amountAsDeCurrency2 = _saleAmountDE.ToString("C", _cultureDE);
-			_amountAsInvariantCurrency2 = _saleAmountDE.ToString("C", _cultureInvariant);
-		}
-
-		private void ButtonC_Click(object sender, RoutedEventArgs e) {
-			// use invariant versions for comparison where culture isn't important
-		
-			ParseDataUS();
-			ParseDataDE();
-			if (_amountAsInvariantCurrency1 == _amountAsInvariantCurrency2)
-			{
-				OutputTextBox3.Text += $"{_amountAsInvariantCurrency1} == {_amountAsInvariantCurrency2}\r\n";
-			}
-			else
-
-			{
-				OutputTextBox3.Text += $"{_amountAsInvariantCurrency1} != {_amountAsInvariantCurrency2}\r\n";
-			}
-		}
-		private void ButtonD_Click(object sender, RoutedEventArgs e) {
-			InputTextBox.Text = "4,615.44";
-			InputTextBoxDE.Text = "4.615,44";
-		}
 		private void ClearButton_Click(object sender, RoutedEventArgs e) {
 			OutputTextBox1.Text = OutputTextBox2.Text = OutputTextBox3.Text = string.Empty;
 		}
