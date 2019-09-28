6eafa8f743ca65341964e688b9a6fd62abfda763
Walt Ritscher <waltritscher@outlook.com>
2019-09-27 23:52:16 -0700

    Add thread culture

--- Source/ExampleApp/MainWindow.xaml.cs
+++ Source/ExampleApp/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,15 +15,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ExampleApp
-{
+namespace ExampleApp {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
+	public partial class MainWindow : Window {
+		public MainWindow() {
 			InitializeComponent();
 		}
 
@@ -29,6 +28,82 @@ namespace ExampleApp
 			OutputTextBox.Text = string.Empty;
 		}
 
+		private void ButtonA_Click(object sender, RoutedEventArgs e) {
+			List<RayPoint> points = new List<RayPoint> { new RayPoint(3, 6), new RayPoint(4, 8) };
+			try
+			{
+				points.Sort();
+			} catch (Exception ex)
+			{
 
+				OutputToScreen(ex.Message);
+			}
+			
+		}
+
+		private void ButtonB_Click(object sender, RoutedEventArgs e) {
+			string[] wordsArray = { "neat", "feét", "féet", "heat" };
+			
+
+			OutputToScreen("Original Array");
+			foreach (var word in wordsArray)
+			{
+				OutputToScreen(word);
+			}
+			Array.Sort(wordsArray);
+			OutputToScreen("-----");
+			OutputToScreen("Sorted Array");
+			foreach (var word in wordsArray)
+			{
+				OutputToScreen(word);
+			}
+		
+		}
+		private void OutputToScreen(string message) {
+
+			OutputTextBox.Text += $"{message}\r\n";
+		}
+
+		private void ButtonC_Click(object sender, RoutedEventArgs e) {
+			var wordsList = new List<string> { "neat", "feét", "féet", "heat", "defeat" };
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+			var query = wordsList.OrderBy(x => x);
+
+			OutputToScreen("Original List");
+			foreach (var word in wordsList)
+			{
+				OutputToScreen(word);
+			}
+
+			OutputToScreen("-----");
+			OutputToScreen("Sorted List (US)");
+
+			foreach (var word in query)
+			{
+				OutputToScreen(word);
+			}
+
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-FR");
+			OutputToScreen("-----");
+			OutputToScreen("Sorted List (FR)");
+			foreach (var word in query)
+			{
+				OutputToScreen(word);
+			}
+
+		}
+	}
+	public class RayPoint {
+
+		public RayPoint() {
+			
+		}
+
+		public RayPoint(int x, int y) {
+			X = x;
+			Y = y;
+		}
+		public int X { get; set; }
+		public int Y { get; set; }
 	}
 }
